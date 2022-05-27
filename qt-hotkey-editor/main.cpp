3255d7547a9e6fe3b7f5fd25bc6716c40e995c9e
Laszlo Papp <laszlo.papp@foundry.com>
2022-05-27 22:24:01 +0100

    Update the style

--- qt-hotkey-editor/main.cpp
+++ qt-hotkey-editor/main.cpp
@@ -5,9 +5,11 @@
 void setApplication(QApplication& application)
 {
     QPalette palette(QColor(50, 50, 50));
-    palette.setBrush(QPalette::Button, QColor(50, 50, 50));
-    palette.setBrush(QPalette::Base, QColor(70, 70, 70));
-    palette.setBrush(QPalette::Highlight, QColor(76, 76, 76));
+    palette.setBrush(QPalette::Button, QColor(77, 77, 77));
+    palette.setBrush(QPalette::Base, QColor(58, 58, 58));
+    palette.setBrush(QPalette::AlternateBase, QColor(53, 53, 53));
+    palette.setBrush(QPalette::Highlight, QColor(0xf7, 0x93, 0x1e));
+    palette.setBrush(QPalette::HighlightedText, QColor(0x1e, 0x1e, 0x1e));
 
     QColor textColor(200, 200, 200);
     palette.setBrush(QPalette::Text, textColor);
