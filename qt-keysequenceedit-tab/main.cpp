be457fb8c8739c750ea5e51694d8d42d8b86c2aa
Laszlo Papp <laszlo.papp@foundry.com>
2022-06-24 16:42:43 +0100

    Update the QKeySequenceEdit tab test bed

--- qt-keysequenceedit-tab/main.cpp
+++ qt-keysequenceedit-tab/main.cpp
@@ -10,14 +10,12 @@ int main(int argc, char *argv[])
 
   QVBoxLayout* layout = new QVBoxLayout;
 
+  // If I comment the button out, I can assign tab to keySequenceEdit
   QPushButton *okButton = new QPushButton("Ok");
   layout->addWidget(okButton);
 
   QKeySequenceEdit* keySequenceEdit = new QKeySequenceEdit();
   layout->addWidget(keySequenceEdit);
-
-  QPushButton *cancelButton = new QPushButton("Cancel");
-  layout->addWidget(cancelButton);
 
   mainWindow.setLayout(layout);
   mainWindow.show();
